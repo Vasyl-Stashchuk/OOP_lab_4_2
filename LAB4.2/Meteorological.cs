9b7c6f44e81e76f71e435d5069155a6fef199d4a
Vasyl-Stashchuk <65713313+Vasyl-Stashchuk@users.noreply.github.com>
2020-06-09 12:49:23 +0300

    Add files via upload

---
+++ LAB4.2/Meteorological.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB4._2
+{
+    public class Meteorological
+    {
+        private string month;
+        private double data;
+        private double data1;
+        private double airtempreture;
+        private double airtempreture1;
+
+        public Meteorological()
+        {
+            month = "None";
+            data = 0;
+            airtempreture = 0;
+        }
+
+        public Meteorological(string month, double data, double data1, double airtemperature, double airtempreture1)
+        {
+            this.month = month;
+            this.data = data;
+            this.data1 = data1;
+            this.airtempreture = airtemperature;
+            this.airtempreture1 = airtempreture1;
+
+        }
+        public Meteorological( int airtemperature, int airtempreture1)
+        {
+           
+            this.airtempreture = airtemperature;
+            this.airtempreture1 = airtempreture1;
+
+        }
+        public string Month
+        {
+            get { return month; }
+            set { month = value; }
+
+        }
+        public double Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+        public double Data1
+        {
+            get { return data1; }
+            set { data1 = value; }
+        }
+        public double AirTemperature
+        {
+            get { return airtempreture; }
+            set { airtempreture = value; }
+        }
+        public double AirTemperature1
+        {
+            get { return airtempreture1; }
+            set { airtempreture1=value; }
+        }
+        public void printResult()
+        {
+            double mid = (airtempreture + airtempreture1) / 2;
+            Console.WriteLine("\n| Month | Average air temperature |");
+            Console.Write("|" + month +  "|" +         mid          + "|");
+       
+        }
+
+        
+
+    }
+}
---
+++ LAB4.2/Program.cs
@@ -0,0 +1,44 @@
+﻿    using System;
+using System.Collections.Generic;
+
+namespace LAB4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter number :");
+            int b = int.Parse(Console.ReadLine());
+            Meteorological[] arr = new Meteorological[b];
+
+            double d;
+            double d1;
+            string month; 
+            double airtemperature;  
+            double airtempreture1;
+            for(int i=0;i<b;i++)
+            {
+                Console.WriteLine("Enter Month");
+                month = Console.ReadLine();
+                Console.WriteLine("Enter 2 days for each month");
+                d = double.Parse(Console.ReadLine());
+                d1 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter airtempetature for 2 days in month ");
+                airtemperature = double.Parse(Console.ReadLine());
+                airtempreture1 = double.Parse(Console.ReadLine());
+
+                arr[i] = new Meteorological(month, d, d1, airtemperature, airtempreture1);
+
+
+            }
+            for(int i =0; i<b; i++)
+            {
+                arr[i].printResult();
+            }
+
+
+        }
+      
+    }
+}
